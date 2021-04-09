0132aeb9054f7e0d3ea84304b1c09f43bf443e6a
Tejausa <tejausa123@gmail.com>
2021-04-09 12:39:24 -0500

    rest of the RESTful API.

--- controllers/food.js
+++ controllers/food.js
@@ -11,8 +11,15 @@ exports.food_list = async function (req, res) {
     //res.send('NOT IMPLEMENTED: food list');
 };
 // for a specific food.
-exports.food_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: food detail: ' + req.params.id);
+exports.food_detail = async function (req, res) {
+    console.log("detail"  + req.params.id)
+    try {
+        result = await food.findById( req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
 // Handle food create on POST.
 // Handle Costume create on POST.
@@ -41,8 +48,22 @@ exports.food_delete = function (req, res) {
     res.send('NOT IMPLEMENTED: food delete DELETE ' + req.params.id);
 };
 // Handle food update form on PUT.
-exports.food_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: food update PUT' + req.params.id);
+exports.food_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body 
+    ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await food.findById( req.params.id)
+        if(req.body.foodtype) toUpdate.foodtype = req.body.foodtype;
+        if(req.body.size) toUpdate.size = req.body.size;
+        if(req.body.cost) toUpdate.cost = req.body.cost;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
+
 };
 
 // VIEWS
